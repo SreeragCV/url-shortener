ef0b84fe9bfaff5520edea79ba2b2ddb16ac17d5
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-11-30 09:52:04 +0530

    created basic frontend

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,6 +1,21 @@
 const shortid = require("shortid");
 const Url = require("../models/url");
 
+module.exports.renderNewForm = (req, res) => {
+  res.render('new')
+}
+
+module.exports.getOneUrl = async (req, res) => {
+  const { id } = req.params
+  const url = await Url.findOne({shortUrl: id})
+  if(url){
+    console.log(url);
+    return res.render('show', {url})
+  } else {
+    return res.status(401).json({error: "Short URL not found"})
+  }
+}
+
 module.exports.createShortUrl = async (req, res) => {
   try {
     const { url } = req.body;
@@ -25,15 +40,15 @@ module.exports.createShortUrl = async (req, res) => {
         shortUrl: urlCode,
         expirationTime: expirationDate,
       });
-      data.save();
-      return res.json(urlCode);
+      await data.save();
+      return res.redirect(`/url/${urlCode}`)
     }
   } catch (err) {
     return res.status(500).json({ error: err });
   }
 };
 
-module.exports.getUrl = async (req, res) => {
+module.exports.fetchOriginalUrl = async (req, res) => {
   try {
     const { shortid } = req.params;
     const findUrl = await Url.findOne({ shortUrl: shortid });
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const mongoose = require('mongoose');
-const {createShortUrl, getUrl} = require('./controllers/urls')
+const {createShortUrl, getOneUrl, fetchOriginalUrl, renderNewForm} = require('./controllers/urls')
 
 app.use(express.json())
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/tinyUrl').
 then(() => {
@@ -13,8 +18,10 @@ then(() => {
     console.log('ERROR CONNECTING TO DATABSE');
 })
 
+app.get('/url/:id', getOneUrl)
+app.get('/create-short-url', renderNewForm)
 app.post('/url', createShortUrl)
-app.get('/:shortid', getUrl)
+app.get('/:shortid', fetchOriginalUrl)
 
 app.listen(3000, () => {
     console.log('LISTENING ON PORT 3000');
